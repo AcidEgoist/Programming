32ba9d382e29b86596874526a1ce0d6d91831210
Artem Lisovskiy <mainegoist@yandex.ru>
2022-10-05 23:05:39 +0300

    Первый коммит

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.Write("Введите число:");
+//string myString = Console.ReadLine();
+Console.WriteLine("{0}  {1}  {2}", 7, 15, 100);
